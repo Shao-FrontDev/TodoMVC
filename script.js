0c43ad2c827a483a3fd9ca5a51993c7e121b6126
Shaodeyu <shaodeyu@foxmail.com>
2021-02-06 17:50:00 +0800

    complete the model module.

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 class Model {
-  constructor() {}
+  constructor() {
+    this.todos = [
+      { id: 1, text: "Run a marathon", complete: false },
+      { id: 2, text: "Plant a garden", complete: false },
+    ];
+  }
+  addTodo(todoText) {
+    const todo = {
+      id:
+        this.todos.length > 0
+          ? this.todos[this.todos.length - 1].id + 1
+          : 1,
+      text: todoText,
+      complete: false,
+    };
+    this.todos.push(todo);
+  }
+
+  edit(id, updatedText) {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id
+        ? {
+            id: todo.id,
+            text: updatedText,
+            complete: false,
+          }
+        : todo
+    );
+  }
+
+  deleteTodo(id) {
+    this.todos = thid.todos.filter(
+      (todo) => todo.id !== id
+    );
+  }
+
+  toggleTodo(id) {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id
+        ? {
+            id: todo.id,
+            text: todo.text,
+            complete: !todo.complete,
+          }
+        : todo
+    );
+  }
 }
 
 class View {
@@ -14,5 +60,3 @@ class Controller {
 }
 
 const app = new Controller(new View(), new Model());
-
-
