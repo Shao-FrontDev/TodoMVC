62e712899dd5b55c94df9605808a7d4add5bcb9b
Shaodeyu <shaodeyu@foxmail.com>
2021-02-06 21:00:35 +0800

    complete the view

--- script.js
+++ script.js
@@ -49,7 +49,39 @@ class Model {
 }
 
 class View {
-  constructor() {}
+  constructor() {
+    this.app = this.getElement("#root");
+
+    this.title = this.createElement("h1");
+    this.title.textContent = "Todos";
+
+    this.form = this.createElement("form");
+
+    this.input = this.createElement("input");
+    this.input.type = "text";
+    this.input.placeholder = "Add todo";
+    this.input.name = "todo";
+
+    this.submitButton = this.createElement("button");
+    this.submitButton.textContent = "Submit";
+
+    this.todoList = this.createElement("ul", "todo-list");
+
+    this.form.append(this.input, this.submitButton);
+
+    this.app.append(this.title, this.form, this.todoList);
+  }
+  createElement(tag, className) {
+    const element = document.createElement(tag);
+    if (className) {
+      element.classList.add(className);
+    }
+    return element;
+  }
+  getElement(selector) {
+    const element = document.querySelector(selector);
+    return element;
+  }
 }
 
 class Controller {
